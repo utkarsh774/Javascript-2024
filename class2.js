1f85154958277bf394595b3d82048e846901ad20
Utkarsh Singh <arsh2502singh@gmail.com>
2024-09-16 17:41:29 +0530

    Execution context

--- class2.js
+++ class2.js
@@ -1,43 +1,72 @@
-// fun()
-// function fun()
+// Execution context in JAVASCRITP
+
+// creates an execution context
+// 2 phases
+// phase 1 -> variable/function memory allocation
+// phase 2 -> actual exection
+
+// stack -> call stack/execution stack/ memory stack/machine stack
+
+// var  n = 2;
+
+// function square(num)
 // {
-//     console.log("I am the real");
-// }
-// fun()
-// function fun()
-// {
-//     console.log("No I am the real");
+//     var ans  = num*num
+//     cube(ans)
+//     return ans;
 
-// }
-// fun()
-// function fun()
-// {
-//     console.log("You both are wrong.I am the real")
-
+//     function cube(ans)
+//     {
+//         console.log(ans*ans*ans)
+//     }
 // }
 
-// fun()
+// var square1 = square(n);
+// var square2 = square(4);
+
+
+
+
+fun()
+function fun()
+{
+    console.log("I am the real");
+}
+fun()
+function fun()
+{
+    console.log("No I am the real");
+
+}
+fun()
+function fun()
+{
+    console.log("You both are wrong.I am the real")
+
+}
+
+fun()
 
 
 
 
 
-// console.log("varName",varName)
-// var varName;
-// console.log("varName",varName)
-// varName = "Captain America"
-// console.log("varName",varName)
-// fun1();
-// function fn()
-// {
-//     console.log("Hello from fn");
-// }
-// fn();
-// fnContainer();
-// var fnContainer = function (){
-//     console.log("I am an Expression")
-// }
-// fnContainer();
+console.log("varName",varName)
+var varName;
+console.log("varName",varName)
+varName = "Captain America"
+console.log("varName",varName)
+fun1();
+function fn()
+{
+    console.log("Hello from fn");
+}
+fn();
+fnContainer();
+var fnContainer = function (){
+    console.log("I am an Expression")
+}
+fnContainer();
 
 
 
